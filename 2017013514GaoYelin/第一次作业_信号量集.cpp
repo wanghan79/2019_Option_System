6ad1f800a82cf51ebe99f0adbc0aa73b19fa23cb
gaoyl093 <767352131@qq.com>
2019-04-11 22:46:13 +0800

    upload

---
+++ 2017013514GaoYelin/第一次作业_信号量集.cpp
@@ -0,0 +1,29 @@
+Swait(S1,t1,d1,…,Sn,tn,dn)
+{
+    while(true)
+    {
+        if(Si>=ti&&…&&Sn>=tn)
+        {
+            for(i=1;i<=n;i++)
+                Si=Si-di;
+            break;
+        }
+        else
+        {
+            调用进程，将进程放置在第一个Si<ti的等待队列，将该处理作为Swait操作的开始
+        }
+    }
+}
+
+
+Ssignal(S1,d1,…,Sn,dn)
+{
+    while(true)
+    {
+        for(i=1;i<=n;i++)
+        {
+            Si=Si+di;
+            取出等待队列中所有与Si相关的进程，放入就绪队列
+        }
+    }
+}
